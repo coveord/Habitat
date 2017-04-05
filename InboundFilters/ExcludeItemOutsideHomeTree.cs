465386bd584e320bde5607730ff6a51860135348
vbernard-coveo <vbernard@coveo.com>
2017-04-05 16:11:30 -0400

    Added App_Config, Processor and InboudFilter

---
+++ InboundFilters/ExcludeItemOutsideHomeTree.cs
@@ -0,0 +1,21 @@
+﻿using Coveo.SearchProvider.Pipelines;
+using Coveo.SearchProvider.InboundFilters;
+
+namespace SitecoreHabitatDemo
+{
+    public class ExcludeItemOutsideHomeTree : AbstractCoveoInboundFilterProcessor
+    {
+        public override void Process(CoveoInboundFilterPipelineArgs args)
+        {
+            if (args.IndexableToIndex != null && !args.IsExcluded && ShouldExecute(args))
+            {
+                var path = args.IndexableToIndex.Item.Paths.FullPath;
+
+                if (path.Contains("Settings") || path.Contains("Global")  || path.Contains("Campaigns"))
+                {
+                    args.IsExcluded = true;
+                }
+            }
+        }
+    }
+}
