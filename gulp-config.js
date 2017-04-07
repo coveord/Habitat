2c8899ce19662bc368529dce718a5ac5eb78d766
vbernard-coveo <vbernard@coveo.com>
2017-04-07 15:55:11 -0400

    Revert "first build"
    This reverts commit d9d1106c4948525122d77ddac957d016389236ec.

--- gulp-config.js
+++ gulp-config.js
@@ -1,14 +1,14 @@
-module.exports = function() {
-    var instanceRoot = "C:\\inetpub\\wwwroot\\Habitat.Coveo.dev.local";
-    var config = {
-        websiteRoot: instanceRoot + "\\Website",
-        sitecoreLibraries: instanceRoot + "\\Website\\bin",
-        licensePath: instanceRoot + "\\Data\\license.xml",
-        solutionName: "Habitat",
-        buildConfiguration: "Debug",
-        buildPlatform: "Any CPU",
-        publishPlatform: "AnyCpu",
-        runCleanBuilds: false
-    };
-    return config;
+module.exports = function () {
+  var instanceRoot = "C:\\websites\\Habitat.Coveo.dev.local";
+  var config = {
+    websiteRoot: instanceRoot + "\\Website",
+    sitecoreLibraries: instanceRoot + "\\Website\\bin",
+    licensePath: instanceRoot + "\\Data\\license.xml",
+    solutionName: "Habitat",
+    buildConfiguration: "Debug",
+    buildPlatform: "Any CPU",
+    publishPlatform: "AnyCpu",
+    runCleanBuilds: false
+  };
+  return config;
 }
